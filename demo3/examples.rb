1cbfe1f60be32e40a612f3e712c6b8c0ef31dfa4
Wes Gamble <weyus@att.net>
2011-08-19 19:12:31 -0500

    Good demos.

--- demo3/examples.rb
+++ demo3/examples.rb
@@ -1,26 +1,34 @@
+VOICE = 'vanessa'
 DEFAULT_OPTIONS = {:attempts    => 2,
                    :timeout     => 15,
-                   :voice       => 'vanessa',
-                   :choices     => [1, 2, 3],
+                   :voice       => VOICE,
+                   :choices     => '1, 2, 3',
                    :onBadChoice => lambda {|event|
                                          case event.attempt
                                            when 1
-                                             say "Bad choice, please try again", { :voice => VOICE }
+                                             say "Bad choice, please try again", {:voice => VOICE}
                                            when 2
-                                             say "Dude, really, it's 1, 2, or 3'", { :voice => VOICE }
+                                             say "Dude, really, it's 1, 2, or 3", {:voice => VOICE}
                                              hangup
                                          end
                                        }}
 
-result = ask 'Press 1 for conference, 2 for transfer, 3 for cool audio', DEFAULT_OPTIONS
+result = ask 'Welcome to Demo 3! Press 1 for conference, 2 for transfer, 3 for cool audio', DEFAULT_OPTIONS
 if result.name == 'choice'
   case result.value
     when '1'
-      say "Conference"
+      say "Welcome to the demo conference"
+      conference "hcc_demo_conference", {:terminator => "*",
+                                         :playTones => true,
+                                         :onChoice => lambda {|event| say("Disconnecting")}}
+      say "Thanks for using the demo conference"
     when '2'
-      say "Transfer"
+      target_phone_number = "2817482498"
+      say "About to transfer this call to #{target_phone_number}"
+      transfer "+1#{target_phone_number}"
     when '3'
-      say "Audio"
+      say "I'm going to play some They Might Be Giants now"
+      say "http://hosting.tropo.com/90387/www/audio/xmliveShadowGovt.mp3"
   end
 end
 
